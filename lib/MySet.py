b2ea9f0d47bde723c5f9271a87dac58b2b918d96
Connor <connordbrown@gmail.com>
2023-11-16 20:32:30 -0700

    complete lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,28 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+    
+    def __str__(self):
+        setStr = ','.join([str(key) for key in self.dictionary.keys()])
+        return f"MySet: {{{setStr}}}"
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+        return self
+        
